49a18504b78b948019208b9cf2f1529135bd5bdd
Juan Monta <jcdbz66@outlook.com>
2026-05-14 16:43:29 -0500

    feat(character-details): implement mat-card and mat-list for details panel
    -implement mat-card preserving original layout

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
@@ -19,7 +21,9 @@ const MATERIAL_COMPONENTS = [
   MatCardModule,
   MatFormFieldModule,
   MatInputModule,
-  MatSelectModule
+  MatSelectModule,
+  MatListModule,
+  MatDividerModule
 ];
 
 @NgModule({
