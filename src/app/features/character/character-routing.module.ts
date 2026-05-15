64ca222df52e2dbfc95541c058eae85e5ac6a17d
Juan Monta <jcdbz66@outlook.com>
2026-05-15 15:54:06 -0500

    refactor(architecture): reorganize folders and rename components based on Smart/Dumb pattern

--- src/app/features/character/character-routing.module.ts
+++ src/app/features/character/character-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CharacterListComponent } from './pages/character-list/character-list.component';
+import { CharacterPageComponent } from './containers/character-page/character-page.component';
 
 const routes: Routes = [
   {
     path: '', // Cuando la ruta sea /characters/'', carga este componente por defecto:
-    component: CharacterListComponent
+    component: CharacterPageComponent
   }
 ];
 
