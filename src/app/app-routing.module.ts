62244540a71fd0eee5e91c5bf97d4e8f586bda17
Juan Monta <jcdbz66@outlook.com>
2026-06-22 12:43:01 -0500

    reafactor(app-routing-module)!: Change app paths to include login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,39 @@ import { CharacterDashboardPageComponent } from './layout/character-dashboard/ch
 import { APP_ROUTES } from './core/constants/routes.dictionary';
 
 export const routes: Routes = [
+  // Redirección Inicial Asegurada: Envía la raíz hacia la característica perezosa (Login)
   {
     path: APP_ROUTES.COMODINS.BLANK_PATH,
+    redirectTo: APP_ROUTES.AUTH.LOGIN,
+    pathMatch: 'full'
+  },
+  // Ruta para el Módulo de Autenticación (Carga Perezosa)
+  {
+    path: APP_ROUTES.AUTH.LOGIN,
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: APP_ROUTES.DASHBOARD.ROOT,
     component: CharacterDashboardPageComponent,
     children: [
       {
-        path: APP_ROUTES.CHARACTERS.ROOT,
-        loadChildren: () => import('./features/character/character.module').then(m => m.CharacterModule)
-      },
-      {
         path: APP_ROUTES.COMODINS.BLANK_PATH,
-        redirectTo: APP_ROUTES.CHARACTERS.ROOT,
-        pathMatch: 'full'
+        loadChildren: () => import('./features/character/character.module').then(m => m.CharacterModule)
       }
     ]
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      // ACTIVACIÓN DE LA ESTRATEGIA DE HASHLOCATIONSTRATEGY
+      useHash: true,
+      // OPTIMIZACIÓN NATIVA DE EXPERIENCIA DE USUARIO Y SCROLLING
+      // para cuando al volver a una pagina anterior, mantiene la posicion del scroll de la pagina antes de avandonarla para ver otra
+      scrollPositionRestoration: 'enabled'
+    }
+  )],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/core/constants/routes.dictionary.ts
+++ src/app/core/constants/routes.dictionary.ts
@@ -1,9 +1,15 @@
 export const APP_ROUTES = {
+  AUTH: {
+    LOGIN: 'login'
+  },
+
   COMODINS: {
     BLANK_PATH: '',
     WILDCARD: '**'
   },
-
+  DASHBOARD: {
+    ROOT: 'dashboard'
+  },
   CHARACTERS: {
     ROOT: 'characters'
   }
