bf16ef171f089b08675920028119bab48315a4c4
Juan Monta <jcdbz66@outlook.com>
2026-06-22 16:32:53 -0500

    feature(auth): Add logout button and session end logic

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiConfigService } from 'src/app/core/services/api-config.service';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
 import { CharacterFavoriteStateFacade } from 'src/app/features/character/facades/character-favorite-state.facade';
 
 @Component({
@@ -10,6 +12,8 @@ import { CharacterFavoriteStateFacade } from 'src/app/features/character/facades
 export class HeaderComponent implements OnInit {
 
   constructor(
+    private readonly authService: AuthService,
+    private readonly notificationService: NotificationService,
     readonly _characterFavoriteStateService: CharacterFavoriteStateFacade,
     readonly apiConfig: ApiConfigService
   ) { }
@@ -22,5 +26,11 @@ export class HeaderComponent implements OnInit {
   toggleCharacterextraInfo() {
     this.showCharacterExtraInfo = !this.showCharacterExtraInfo;
   }
+
+  public onLogout(): void {
+    this.notificationService.showWarning('Sesión de la Ciudadela Finalizada Correctamente.');
+    this.authService.logout();
+  }
+
 
 }
