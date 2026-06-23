c1d53d07b3980ecede21f06abac7689dbc45c3a8
Juan Monta <jcdbz66@outlook.com>
2026-06-23 14:52:25 -0500

    feature(auth-guard)!: Add auth guard file
    -Adding authentication guard file to allow the user to enter the dashboard route.
    
    -Updating app-routing.module.ts to use the auth-portal.guard.ts in the dashboard path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 import { APP_ROUTES } from './core/constants/routes.dictionary';
 import { AppRoutes } from './core/models/routing.model';
 import { CharacterDashboardPageComponent } from './layout/character-dashboard/character-dashboard-page.component';
+import { AuthPortalGuard } from './core/guards/auth-portal.guard';
 
 export const routes: AppRoutes = [
   // Redirección Inicial Asegurada: Envía la raíz hacia la característica perezosa (Login)
@@ -19,6 +20,13 @@ export const routes: AppRoutes = [
   {
     path: APP_ROUTES.DASHBOARD.ROOT,
     component: CharacterDashboardPageComponent,
+    data: {
+      requiresAuth: true
+    },
+    // Mutiples guards que se activarán secuencialmente segun el array, solo uno que de false se detendrá todo ahí sin proseguir con los demas guards
+    canActivate: [
+      AuthPortalGuard, // Guard para verificar si tiene authorizacion para ingresar al path
+    ],
     children: [
       {
         path: APP_ROUTES.COMODINS.BLANK_PATH,
