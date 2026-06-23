2bb223989cafa641a7976e572abec4c8c4a93d50
Juan Monta <jcdbz66@outlook.com>
2026-06-23 17:10:46 -0500

    style(api-toggle-button): Add angular material toggle button for change between rest or graphql
    -Adding material angular MatSlideToggleModule component in material.module.ts
    
    -Removing unused css code

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -10,6 +10,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 
@@ -25,8 +26,9 @@ const MATERIAL_COMPONENTS = [
   MatPaginatorModule,
   MatProgressSpinnerModule,
   MatSelectModule,
+  MatSlideToggleModule,
   MatSnackBarModule,
-  MatTableModule
+  MatTableModule,
 ];
 
 @NgModule({
