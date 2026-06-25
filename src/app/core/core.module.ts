13455c4fa4779b1758d95957f9f1bd54f0ce0d36
Juan Monta <jcdbz66@outlook.com>
2026-06-25 12:39:46 -0500

    refactor(core-moduel): Create core module
    -Cleaning app module by isolating services and components into core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpCacheInterceptor } from './interceptors/http-cache.interceptor';
+import { ApiAuditInterceptor } from './interceptors/api-audit.interceptor';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+  ],
+  providers: [
+    //===================================================================================
+    // ** Aquí dependiendo del orden de colocar primero el Inteerceptor de Audit o el cache, tendremos dos escenarios:
+    // ** Escenario A: Auditoría ANTES que Caché ([ApiAuditInterceptor, HttpCacheInterceptor])
+    /*La app dispara la petición → Auditoría dice "¡Despegue 🛫!" → Caché la intercepta,
+      ve que ya la tiene y la devuelve sin salir a internet → Auditoría dice "¡Aterrizaje 🛬!".
+
+      -Resultado: Se verán logs de todas las peticiones que haga la aplicación, pero se notará que
+       las que vienen de la caché tendrán un tiempo de tránsito ridículamente bajo (ej. 0.5ms),
+       mientras que las reales tardarán más (ej. 250ms).
+       Es la configuración más recomendada si se quiere medir cuánto tiempo nos estamos ahorrando.*/
+
+    // ** Escenario B: Caché ANTES que Auditoría ([HttpCacheInterceptor, ApiAuditInterceptor])
+    /* La app dispara la petición → Caché la intercepta, ve que ya la tiene y la devuelve inmediatamente a la app.
+      -Resultado: El interceptor de auditoría nunca se entera de que hubo una petición si esta fue resuelta por la caché.
+      Solo se verá los mensajes de auditoría en la consola para las peticiones reales que sí viajan por internet.
+      Es ideal si se quiere una consola más limpia.
+    */
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpCacheInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiAuditInterceptor,
+      multi: true // Indica que es un eslabón más en la cadena de interceptores nativos
+    },
+    //===================================================================================
+  ]
+})
+export class CoreModule { }
