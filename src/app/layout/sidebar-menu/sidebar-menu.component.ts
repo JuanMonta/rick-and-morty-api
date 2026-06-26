3c0c79914b8f120d786b274e356bd67c9afade48
Juan Monta <jcdbz66@outlook.com>
2026-06-26 14:40:01 -0500

    refactor(sidebar-menu): Add dinamic menu items
    -Creating default menu items with acess role
    
    -Adding menu items based on user rol.

--- src/app/layout/sidebar-menu/sidebar-menu.component.ts
+++ src/app/layout/sidebar-menu/sidebar-menu.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { User } from 'src/app/core/models/user.model';
+import { map } from 'rxjs/operators';
+import { APP_ROUTES } from 'src/app/core/constants/routes.dictionary';
+import { Role, User, UserRole } from 'src/app/core/models/user.model';
 import { AuthService } from 'src/app/core/services/auth.service';
+
+interface MenuItem {
+  label: string;
+  icon: string;
+  route: string;
+  allowedRoles: UserRole[];
+}
 
 @Component({
   selector: 'app-sidebar-menu',
@@ -11,11 +20,48 @@ import { AuthService } from 'src/app/core/services/auth.service';
 export class SidebarMenuComponent implements OnInit {
 
   public currentUser$: Observable<User | null> = this.authService.currentUser$;
+
+  private readonly defaultMenuItems: MenuItem[] = [
+    {
+      label: 'Dashboard',
+      icon: 'dashboard',
+      route: `/${APP_ROUTES.DASHBOARD.ROOT}`,
+      allowedRoles: [Role.ADMIN, Role.SCIENTIST, Role.GUEST]
+    },
+    {
+      label: 'Personajes',
+      icon: 'face',
+      route: `/${APP_ROUTES.ERRORS.ACCESS_DENIED}`,
+      allowedRoles: [Role.ADMIN, Role.SCIENTIST]
+    },
+    {
+      label: 'Auditoría',
+      icon: 'shield',
+      route: `/${APP_ROUTES.ERRORS.NOT_FOUND}`,
+      allowedRoles: [Role.ADMIN]
+    },
+    {
+      label: 'Configuración',
+      icon: 'settings',
+      route: `/${APP_ROUTES.ERRORS.ACCESS_DENIED}`,
+      allowedRoles: [Role.ADMIN]
+    }
+  ]
+
+  public menuItems$!: Observable<MenuItem[]>;
+
   constructor(
     private readonly authService: AuthService
   ) { }
 
   ngOnInit(): void {
+    this.menuItems$ = this.currentUser$.pipe(
+      map(user => {
+        if (!user) return [];
+        return this.defaultMenuItems.filter(item => item.allowedRoles.includes(user.role))
+      })
+    );
   }
+
 
 }
