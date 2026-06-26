1fb663bd5375a85b2d94321c913e89004a38d815
Juan Monta <jcdbz66@outlook.com>
2026-06-26 14:31:51 -0500

    refactor(error-components)!: Update action buttons
    -Update goBack button from access denied component
    
    -Remove go to dashboard button from not found component
    
    -Add style to both access denied and not found component.

--- src/app/core/components/access-denied/access-denied.component.ts
+++ src/app/core/components/access-denied/access-denied.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { NavigationHistoryService } from '../../services/navigation-history.service';
 import { Location } from '@angular/common';
-import { APP_ROUTES } from '../../constants/routes.dictionary';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-access-denied',
@@ -11,24 +8,18 @@ import { APP_ROUTES } from '../../constants/routes.dictionary';
 })
 export class AccessDeniedComponent implements OnInit {
 
-  hasInternalHistory: string | null = null;
 
   constructor(
-    private readonly router: Router,
-    private readonly navigationHistoryService: NavigationHistoryService,
+
     private readonly location: Location
   ) {
   }
 
   ngOnInit(): void {
-    this.hasInternalHistory = this.navigationHistoryService.getPreviousUrl();
+
   }
 
   public goBack(): void {
-    if (this.hasInternalHistory) {
-      this.location.back();
-    } else {
-      this.router.navigate(['/', APP_ROUTES.DASHBOARD.ROOT]);
-    }
+    this.location.back();
   }
 }
